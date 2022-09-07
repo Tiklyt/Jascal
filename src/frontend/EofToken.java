f7d162aec8ad15b92f7f53be5e9e471b76a970b5
Bilal Brarou <braroubilal@gmail.com>
2022-09-07 20:15:20 +0200

    EofToken, Token and TokenType impl.

---
+++ src/frontend/EofToken.java
@@ -0,0 +1,13 @@
+package frontend;
+
+public class EofToken extends  Token {
+
+
+    public EofToken(Source source) throws Exception {
+        super(source);
+    }
+
+    protected void extract() throws Exception{
+
+    }
+}
---
+++ src/frontend/Token.java
@@ -0,0 +1,35 @@
+package frontend;
+
+public class Token {
+    protected TokenType type;
+    protected String text;
+    protected Object value;
+    protected Source source;
+    protected int lineNum;
+    protected int position;
+
+    public Token(Source source) throws Exception {
+        this.source =source;
+        this.lineNum = source.getLineNum();
+        this.lineNum = source.getPosition();
+        extract();
+    }
+
+    protected void extract() throws Exception {
+        text = Character.toString(currentChar());
+        value = null;
+        nextChar();
+    }
+
+    protected char currentChar() throws Exception {
+        return source.currentChar();
+    }
+
+    protected  char nextChar() throws Exception{
+        return source.nextChar();
+    }
+
+    protected char peekChar() throws Exception{
+        return source.peekChar();
+    }
+}
