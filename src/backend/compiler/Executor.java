f8c39c60a51d5bfd2c59b764c08dc9a83e860653
Bilal Brarou <braroubilal@gmail.com>
2022-10-12 21:33:28 +0200

    Executor class creation

--- src/backend/compiler/Executor.java
+++ src/backend/compiler/Executor.java
@@ -1,4 +1,20 @@
 package backend.compiler;
 
-public class Executor{
+import intermediate.ICode;
+import intermediate.SymTab;
+import message.Message;
+import message.MessageHandler;
+import message.MessageType;
+
+public class Executor extends MessageHandler {
+
+    public void process(ICode iCode, SymTab symTab){
+        long startTime = System.currentTimeMillis();
+        float elapsedTime = (System.currentTimeMillis() - startTime) / 1000f;
+        int executionCount = 0;
+        int runtimeErrors = 0;
+
+        sendMessage(new Message(MessageType.INTERPRETER_SUMMARY,new Number[]{executionCount,runtimeErrors,elapsedTime}));
+
+    }
 }
